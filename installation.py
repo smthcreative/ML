0fab879d15b04711e5bb787aedca03da888e0bf3
smthcreative <93734464+smthcreative@users.noreply.github.com>
2024-04-26 09:13:40 +0300

    Add files via upload

---
+++ installation.py
@@ -0,0 +1,11 @@
+import os
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+req_file_path = os.path.join(base_dir, 'layer_divider_requirements.txt')
+from launch import is_installed, run_pip
+with open(req_file_path) as file:
+    for package in file:
+        package = package.strip()
+        if not is_installed(package):
+            run_pip(
+                f"install {package}", f"Layer Divider Extension: Installing  {package}")
